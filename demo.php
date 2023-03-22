ff7650f25c7773ef142fbe13faf858ae66dc7447
Philo Hermans <me@philo.dev>
2023-03-22 16:17:23 +0100

    Initial commit

---
+++ demo.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name: Demo
+Plugin URI: http://anystack.sh
+Description: Example plugin
+Author: Philo Hermans
+Version: REF
+Author URI: https://anystack.sh
+*/
+
+require 'wp-guard/src/WpGuard.php';
+
+$guard = new Anystack\WpGuard\V001\WpGuard(
+	__FILE__,
+	[
+		'api_key' => '<API KEY>',
+		'product_id' => '<PRODUCT ID>',
+		'product_name' => 'Demo',
+		'license' => [
+			'require_email' => true,
+		],
+		'updater' => [
+			'enabled' => true,
+		]
+	]
+);
+
+$guard->validCallback(function() {
+	add_action( 'admin_notices', 'daily_revenue' );
+});
+
+function daily_revenue() {
+	echo '<p>Your revenue for today is $123.45</p>';
+}
